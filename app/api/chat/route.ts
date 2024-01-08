80244519823326244cff2cc26ed943ddfaa3d897
santohack <santohack238@gmail.com>
2024-01-08 15:05:07 +0530

    chat commented

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,50 +1,50 @@
-import { notesIndex } from "@/lib/db/pinecone"
-import openai, { getEmbedding } from "@/lib/openai"
-import { auth } from "@clerk/nextjs"
-import { ChatCompletionMessage } from "openai/resources/index.mjs"
-import prisma from "@/lib/db/prisma"
-import { OpenAIStream, StreamingTextResponse } from "ai"
-export const POST = async (req: Request) => {
-    try {
-        const body = await req.json()
-        const messages: ChatCompletionMessage[] = body.messages
-        const messageTurnCate = messages.slice(-6)
-        const embedding = await getEmbedding(
-            messageTurnCate.map((message) => message.content).join("\n")
-        )
-        const { userId } = auth()
-        const vectorQueryResponse = await notesIndex.query({
-            vector: embedding,
-            topK: 30,
-            filter: {
-                userId
-            }
-        })
+// import { notesIndex } from "@/lib/db/pinecone"
+// import openai, { getEmbedding } from "@/lib/openai"
+// import { auth } from "@clerk/nextjs"
+// import { ChatCompletionMessage } from "openai/resources/index.mjs"
+// import prisma from "@/lib/db/prisma"
+// import { OpenAIStream, StreamingTextResponse } from "ai"
+// export const POST = async (req: Request) => {
+//     try {
+//         const body = await req.json()
+//         const messages: ChatCompletionMessage[] = body.messages
+//         const messageTurnCate = messages.slice(-6)
+//         const embedding = await getEmbedding(
+//             messageTurnCate.map((message) => message.content).join("\n")
+//         )
+//         const { userId } = auth()
+//         const vectorQueryResponse = await notesIndex.query({
+//             vector: embedding,
+//             topK: 30,
+//             filter: {
+//                 userId
+//             }
+//         })
 
-        const releventNotes = await prisma.note.findMany({
-            where: {
-                id: {
-                    in: vectorQueryResponse.matches.map((note) => note.id)
-                }
-            }
+//         const releventNotes = await prisma.note.findMany({
+//             where: {
+//                 id: {
+//                     in: vectorQueryResponse.matches.map((note) => note.id)
+//                 }
+//             }
 
-        })
-        console.log(releventNotes, 'releventNotes');
-        const systemMessege: ChatCompletionMessage = {
-            role: "assistant",
-            content: "You are an ai assistant. You are helpful, creative, clever, and very friendly." +
-                releventNotes.map((note) => `Title : ${note.title}\n\nContent:\n${note.content}`).join("\n\n"),
-        }
-        const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            stream: true,
-            messages: [systemMessege, ...messageTurnCate],
-        })
+//         })
+//         console.log(releventNotes, 'releventNotes');
+//         const systemMessege: ChatCompletionMessage = {
+//             role: "assistant",
+//             content: "You are an ai assistant. You are helpful, creative, clever, and very friendly." +
+//                 releventNotes.map((note) => `Title : ${note.title}\n\nContent:\n${note.content}`).join("\n\n"),
+//         }
+//         const response = await openai.chat.completions.create({
+//             model: "gpt-3.5-turbo",
+//             stream: true,
+//             messages: [systemMessege, ...messageTurnCate],
+//         })
 
-        const stream = OpenAIStream(response)
-        return new StreamingTextResponse(stream)
-    } catch (error) {
-        console.log(error);
-        return Response.json({ error: "Invalid request" }, { status: 400 });
-    }
-}
+//         const stream = OpenAIStream(response)
+//         return new StreamingTextResponse(stream)
+//     } catch (error) {
+//         console.log(error);
+//         return Response.json({ error: "Invalid request" }, { status: 400 });
+//     }
+// }
