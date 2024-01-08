49cbb471cce610eed0a795cbfa6fb78c1c35cef4
santohack <santohack238@gmail.com>
2024-01-08 15:02:34 +0530

    removed ar functioality

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,51 +4,47 @@ import { auth } from "@clerk/nextjs"
 import { ChatCompletionMessage } from "openai/resources/index.mjs"
 import prisma from "@/lib/db/prisma"
 import { OpenAIStream, StreamingTextResponse } from "ai"
-export const POST = async (req: Request): Promise<Response> => {
+export const POST = async (req: Request) => {
     try {
-        const body = await req.json();
-        const messages: ChatCompletionMessage[] = body.messages;
-        const messageTurnCate = messages.slice(-6);
+        const body = await req.json()
+        const messages: ChatCompletionMessage[] = body.messages
+        const messageTurnCate = messages.slice(-6)
         const embedding = await getEmbedding(
-            messageTurnCate.map((message) => message.content).join('\n')
-        );
-        const { userId } = auth();
-
+            messageTurnCate.map((message) => message.content).join("\n")
+        )
+        const { userId } = auth()
         const vectorQueryResponse = await notesIndex.query({
             vector: embedding,
             topK: 30,
             filter: {
-                userId,
-            },
-        });
+                userId
+            }
+        })
 
         const releventNotes = await prisma.note.findMany({
             where: {
                 id: {
-                    in: vectorQueryResponse.matches.map((note) => note.id),
-                },
-            },
-        });
+                    in: vectorQueryResponse.matches.map((note) => note.id)
+                }
+            }
 
-        const systemMessage: ChatCompletionMessage = {
-            role: 'assistant',
-            content:
-                'You are an AI assistant. You are helpful, creative, clever, and very friendly.' +
-                releventNotes
-                    .map((note) => `Title : ${note.title}\n\nContent:\n${note.content}`)
-                    .join('\n\n'),
-        };
-
+        })
+        console.log(releventNotes, 'releventNotes');
+        const systemMessege: ChatCompletionMessage = {
+            role: "assistant",
+            content: "You are an ai assistant. You are helpful, creative, clever, and very friendly." +
+                releventNotes.map((note) => `Title : ${note.title}\n\nContent:\n${note.content}`).join("\n\n"),
+        }
         const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
+            model: "gpt-3.5-turbo",
             stream: true,
-            messages: [systemMessage, ...messageTurnCate],
-        });
+            messages: [systemMessege, ...messageTurnCate],
+        })
 
-        const stream = OpenAIStream(response);
-        return new StreamingTextResponse(stream);
+        const stream = OpenAIStream(response)
+        return new StreamingTextResponse(stream)
     } catch (error) {
         console.log(error);
-        return new Response({ error: 'Invalid request' }, { status: 400 });
+        return Response.json({ error: "Invalid request" }, { status: 400 });
     }
-};
+}
--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,15 +1,155 @@
+// import { noteDeleteSchema, noteSchema, noteUpdateSchema } from "@/lib/validation/note"
+// import { auth } from "@clerk/nextjs";
+// import prisma from "@/lib/db/prisma"
+// import { getEmbedding } from "@/lib/openai";
+// import { notesIndex } from "@/lib/db/pinecone"
+// import { title } from "process";
+
+
+// const getEmbaddingForNotes = async (title: string, content: string | undefined) => {
+//     return getEmbedding(title + "\n\n" + content ?? "");
+// }
+
+// export const POST = async (req: Request) => {
+//     try {
+//         const body = await req.json()
+
+//         const parseResult = noteSchema.safeParse(body)
+//         if (!parseResult.success) {
+//             console.log(parseResult.error);
+//             return Response.json({ error: parseResult.error }, { status: 400 })
+//         }
+        
+//         const { title, content } = parseResult.data
+//         const { userId } = auth()
+//         if (!userId) {
+//             return Response.json({ error: 'Unauthorized' }, { status: 401 })
+//         }
+        
+//         const embedding = await getEmbaddingForNotes(title, content)
+//         const note = await prisma.$transaction(async (tx) => {
+//             const createdNote = await tx.note.create({
+//                 data: {
+//                     title,
+//                     content,
+//                     userId
+//                 },
+//             })
+
+//             await notesIndex.upsert([{
+//                 id: createdNote.id,
+//                 values: embedding,
+//                 metadata: { userId }
+//             }])
+            
+//             return createdNote
+//         })
+
+//         return Response.json({ note }, { status: 200 })
+//     } catch (error) {
+//         console.error('Error occurred:', error);
+//         return Response.json({ error: 'Internal server error' }, { status: 500 })
+//     }
+// }
+
+
+// export const PATCH = async (req: Request) => {
+//     try {
+//         const body = await req.json()
+//         const parseResult = noteUpdateSchema.safeParse(body)
+//         if (!parseResult.success) {
+//             console.log(parseResult.error);
+//             return Response.json({ error: parseResult.error }, { status: 400 })
+//         }
+//         const { id, title, content } = parseResult.data
+
+//         const note = await prisma.note.findUnique({
+//             where: {
+//                 id
+//             }
+//         })
+
+//         if (!note) {
+//             return Response.json({ error: 'Note not found' }, { status: 404 })
+//         }
+//         const { userId } = auth()
+//         if (!userId && userId !== note.userId) {
+//             return Response.json({ error: 'Unauthorized' }, { status: 401 })
+//         }
+
+//         const embedding = await getEmbaddingForNotes(title, content)
+//         const updateNote = await prisma.$transaction(async (tx) => {
+//             const updated = await tx.note.update({
+//                 where: {
+//                     id
+//                 },
+//                 data: {
+//                     title,
+//                     content
+//                 }
+//             })
+//             await notesIndex.upsert([{
+//                 id,
+//                 values: embedding,
+//                 metadata: { userId }
+//             }])
+//             return updated
+//         })
+       
+//         return Response.json({ updateNote }, { status: 200 })
+//     } catch (error) {
+//         console.log(error);
+//         return Response.json({ error: 'internal server' }, { status: 500 })
+//     }
+// }
+
+// export const DELETE = async (req: Request) => {
+//     try {
+
+//         const body = await req.json()
+//         const parseResult = noteDeleteSchema.safeParse(body)
+//         if (!parseResult.success) {
+//             console.log(parseResult.error);
+//             return Response.json({ error: parseResult.error }, { status: 400 })
+//         }
+//         const { id } = parseResult.data
+//         const note = await prisma.note.findUnique({
+//             where: {
+//                 id
+//             }
+//         })
+
+//         if (!note) {
+//             return Response.json({ error: 'Note not found' }, { status: 404 })
+//         }
+//         const { userId } = auth()
+//         if (!userId && userId !== note.userId) {
+//             return Response.json({ error: 'Unauthorized' }, { status: 401 })
+//         }
+
+//        await prisma.$transaction(async (tx) => {
+//             await tx.note.delete({
+//                 where: {
+//                     id
+//                 }
+//             })
+//             await notesIndex.deleteOne([id])
+//        })
+      
+//         return Response.json({ message: 'Note deleted successfully' }, { status: 200 })
+//     } catch (error) {
+//         console.log(error);
+//         return Response.json({ error: "internal server" }, { status: 500 })
+//     }
+// }
+
+
+
+///normal crud opertion
+
 import { noteDeleteSchema, noteSchema, noteUpdateSchema } from "@/lib/validation/note"
 import { auth } from "@clerk/nextjs";
 import prisma from "@/lib/db/prisma"
-import { getEmbedding } from "@/lib/openai";
-import { notesIndex } from "@/lib/db/pinecone"
-import { title } from "process";
-
-
-const getEmbaddingForNotes = async (title: string, content: string | undefined) => {
-    return getEmbedding(title + "\n\n" + content ?? "");
-}
-
 export const POST = async (req: Request) => {
     try {
         const body = await req.json()
@@ -19,39 +159,26 @@ export const POST = async (req: Request) => {
             console.log(parseResult.error);
             return Response.json({ error: parseResult.error }, { status: 400 })
         }
-        
         const { title, content } = parseResult.data
         const { userId } = auth()
         if (!userId) {
             return Response.json({ error: 'Unauthorized' }, { status: 401 })
         }
-        
-        const embedding = await getEmbaddingForNotes(title, content)
-        const note = await prisma.$transaction(async (tx) => {
-            const createdNote = await tx.note.create({
-                data: {
-                    title,
-                    content,
-                    userId
-                },
-            })
 
-            await notesIndex.upsert([{
-                id: createdNote.id,
-                values: embedding,
-                metadata: { userId }
-            }])
-            
-            return createdNote
+        const note = await prisma.note.create({
+            data: {
+                title,
+                content,
+                userId
+            }
         })
 
         return Response.json({ note }, { status: 200 })
     } catch (error) {
-        console.error('Error occurred:', error);
-        return Response.json({ error: 'Internal server error' }, { status: 500 })
+        return Response.json({ error: 'internal server' }, { status: 500 })
+
     }
 }
-
 
 export const PATCH = async (req: Request) => {
     try {
@@ -62,7 +189,6 @@ export const PATCH = async (req: Request) => {
             return Response.json({ error: parseResult.error }, { status: 400 })
         }
         const { id, title, content } = parseResult.data
-
         const note = await prisma.note.findUnique({
             where: {
                 id
@@ -76,26 +202,15 @@ export const PATCH = async (req: Request) => {
         if (!userId && userId !== note.userId) {
             return Response.json({ error: 'Unauthorized' }, { status: 401 })
         }
-
-        const embedding = await getEmbaddingForNotes(title, content)
-        const updateNote = await prisma.$transaction(async (tx) => {
-            const updated = await tx.note.update({
-                where: {
-                    id
-                },
-                data: {
-                    title,
-                    content
-                }
-            })
-            await notesIndex.upsert([{
-                id,
-                values: embedding,
-                metadata: { userId }
-            }])
-            return updated
+        const updateNote = await prisma.note.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                content
+            }
         })
-       
         return Response.json({ updateNote }, { status: 200 })
     } catch (error) {
         console.log(error);
@@ -126,16 +241,11 @@ export const DELETE = async (req: Request) => {
         if (!userId && userId !== note.userId) {
             return Response.json({ error: 'Unauthorized' }, { status: 401 })
         }
-
-       await prisma.$transaction(async (tx) => {
-            await tx.note.delete({
-                where: {
-                    id
-                }
-            })
-            await notesIndex.deleteOne([id])
-       })
-      
+        await prisma.note.delete({
+            where: {
+                id
+            }
+        })
         return Response.json({ message: 'Note deleted successfully' }, { status: 200 })
     } catch (error) {
         console.log(error);
